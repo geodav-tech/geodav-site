eba47b98d3accc7f735387406829edda53cfef34
Mike Davlantes <rocks722@hotmail.com>
2022-03-10 14:06:43 -0700

    Revert "WIP: testing collections"
    44b9dd83cee2364576d31f7d1307117bffc28a9c

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,29 +6,5 @@
 
 module.exports = {
   siteName: 'geodav.tech',
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'services/**/*.md',
-        typeName: 'Service',
-        resolveAbsolutePaths: true,
-        remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
-        }
-      }
-    }
-  ],
-  templates: {
-    Service: '/services/:slug'
-  },
-  transformers: {
-    remark: {
-      autolinkClassName: 'icon icon-link heading-anchor',
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['noopener', ],
-      anchorClassName: 'icon icon-link',
-    }
-  }
+  plugins: []
 }
