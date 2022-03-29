981f9cc26d143705b85d31da3b216201138fdce5
Mike Davlantes <rocks722@hotmail.com>
2022-03-29 14:45:46 -0600

    better error handling to make sure i don't forget to bundle icons

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -22,7 +22,16 @@ export default {
     iconMarkup() {
       // if it's an mdi icon, you can pass @mdi<icon_name> and we'll fetch the file from icon-filter.js, eg @mdiChevronRight
       // otherwise you must pass complete SVG path markup that you've already fetched from a file somewhere.
-      return this.$props.icon.includes('@mdi') ? require('./icon-filter')[this.$props.icon.replace('@', '')] : this.$props.icon
+      if (this.$props.icon.includes('@mdi')) {
+        let fetchedIcon = require('./icon-filter')[this.$props.icon.replace('@', '')]
+        if (fetchedIcon) {
+          return fetchedIcon
+        } else {
+          throw `${this.$props.icon} was not found in icon-filter.js. Did you forget to export it?`
+        }
+      } else {
+        return this.$props.icon
+      }
     }
   }
 }
