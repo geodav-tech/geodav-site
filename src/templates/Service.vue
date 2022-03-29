34dd6eccef0f80556ed4a6c091e55c4335b9c70e
Mike Davlantes <rocks722@hotmail.com>
2022-03-29 11:42:43 -0600

    WIP single service

--- src/templates/Service.vue
+++ src/templates/Service.vue
@@ -1,16 +1,37 @@
 <template>
   <Layout>
-    <h1>{{ $page.service.title }}</h1>
-    <p class="intro">{{ $page.service }}</p>
-    <VueRemarkContent />
+    <section class="my-5 px-4">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h1 class="mb-4">{{$page.service.title}}</h1>
+            <div class="section-header-line" style="margin-left: 0;"></div>
+          </div>
+        </div>
+        <VueRemarkContent />
+      </div>
+    </section>
   </Layout>
 </template>
-
-<!-- Front-matter fields can be queried from GraphQL layer -->
 <page-query>
 query Service ($id: ID!) {
   service(id: $id) {
     title
+    tagline
+    summary
+    coverImage
   }
 }
 </page-query>
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.service.title
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
