9785e94915f3577f77196df23cdf62112c6250ae
Mike Davlantes <rocks722@hotmail.com>
2022-05-07 09:11:52 -0500

    WIP portfolio item: needs letterboxing for media, probably a testimonial too

--- src/templates/PortfolioItem.vue
+++ src/templates/PortfolioItem.vue
@@ -1,8 +1,49 @@
 <template>
   <Layout>
-    <h1>{{ $page.portfolioItem.title }}</h1>
-    <p class="intro">{{ $page.portfolioItem }}</p>
-    <VueRemarkContent />
+    <section class="my-5 px-4">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1 class="mb-4" style="text-wrap: balance;">{{$page.portfolioItem.title}}</h1>
+            <div class="section-header-line" style="margin-left: 0;"></div>
+            <div class="row">
+              <div class="col-md-6 mb-4 text-center">
+                <g-image loading="lazy" class="img-fluid" :src="$page.portfolioItem.coverImage" :alt="$page.portfolioItem.title" />
+                <primary-cta
+                  v-if="$page.portfolioItem.demoUrl"
+                  class="m-3"
+                  text="SEE IT IN ACTION!"
+                  :route="$page.portfolioItem.demoUrl"
+                  target="_blank"
+                />
+              </div>
+              <div class="col-md-6 mb-4 pe-md-3">
+                <h4>{{$page.portfolioItem.tagline}}</h4>
+                <ul class="ms-2" style="font-size: 1.3rem">
+                  <li
+                    v-for="(text, ind) in $page.portfolioItem.summary"
+                    :key="`${ind}${text}`"
+                    v-html="text"
+                    class="mb-2"
+                  />
+                </ul>
+              </div>
+              <div class="row d-flex justify-content-center">
+                <div class="col-md-4 p-2" v-for="image in $page.portfolioItem.additionalImages" :key="image.src">
+                  <g-image loading="lazy" class="img-fluid img-thumbnail" :src="image" alt="$page.portfolioItem.title" />
+                </div>
+              </div>
+              <!-- <testimonial-card v-bind="$page.portfolioItem.testimonial"/> -->
+            </div>
+            <VueRemarkContent class="pe-md-3"/>
+          </div>
+          <contact-sidebar
+            spanCallout="Can we build something like this to improve your workflows?"
+            contactTagline="Get in touch and let's have a chat about how we can help!"
+          />
+        </div>
+      </div>
+    </section>
   </Layout>
 </template>
 
@@ -11,6 +52,36 @@
 query PortfolioItem ($id: ID!) {
   portfolioItem(id: $id) {
     title
+    coverImage
+    demoUrl
+    tagline
+    summary
+    servicesUsed(sortBy: "index", order: ASC) {
+      id
+    }
+    testimonial {
+      name
+      url
+      title
+      quote
+      starCount
+      photo
+    }
+    additionalImages
   }
 }
 </page-query>
+<script>
+import ContactSidebar from '../components/ContactSidebar'
+import PrimaryCta from '../components/PrimaryCta'
+import TestimonialCard from '../components/TestimonialCard'
+
+export default {
+	components: { ContactSidebar, PrimaryCta, TestimonialCard },
+  metaInfo() {
+    return {
+      title: this.$page.portfolioItem.title
+    }
+  }
+}
+</script>
