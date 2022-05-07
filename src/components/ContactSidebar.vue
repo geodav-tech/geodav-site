11311463be7a32f70243d374d528439362047218
Mike Davlantes <rocks722@hotmail.com>
2022-05-07 09:11:12 -0500

    componetized the contact sidebar

---
+++ src/components/ContactSidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="p-3 side-bar" :class="colClasses">
+    <span-callout>
+      {{spanCallout}}
+    </span-callout>
+    <div class="section-header-line my-3"></div>
+    <div class="mb-3 bold contact-tagline text-center">
+      {{contactTagline}}
+    </div>
+    <contact-form/>
+  </div>
+</template>
+<script>
+import ContactForm from '../components/ContactForm'
+import SpanCallout from '../components/SpanCallout'
+
+export default {
+	components: { SpanCallout, ContactForm },
+  props: {
+    spanCallout: String,
+    contactTagline: String,
+    colClasses: { // bootstrap col sizes; get plugged in as classnames
+      type: String,
+      default: 'col-md-4'
+    }
+  }
+}
+</script>
+<style scoped>
+.side-bar {
+  /* this is a lighter geodav-grey */
+  background-color: #7f7f7f14;
+  border-radius: .25rem;
+  height: fit-content;
+}
+.contact-tagline {
+  font-size: 1.2rem;
+  color: var(--geodav-red);
+}
+</style>
